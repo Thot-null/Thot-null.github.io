5835228bf6c0b571c27d01187604f1819cf9e585
Thot-null <3277655825@qq.com>
2023-06-27 23:41:24 +0800

    Site updated: 2023-06-27 23:41:22

--- js/myself_settings.js
+++ js/myself_settings.js
@@ -1,1 +1,273 @@
-function getDistance(e,a,s,t){const{sin:r,cos:n,asin:c,PI:o,hypot:i}=Math;let k=(e,a)=>(e*=o/180,{x:n(a*=o/180)*n(e),y:n(a)*r(e),z:r(a)}),b=k(e,a),u=k(s,t),p=2*c(i(b.x-u.x,b.y-u.y,b.z-u.z)/2)*6371;return Math.round(p)}function showWelcome(){if(!document.getElementById("welcome-info"))return;let e,a,s=getDistance(110.84721,35.97053,ipLoacation.result.location.lng,ipLoacation.result.location.lat),t=ipLoacation.result.ad_info.nation;switch(ipLoacation.result.ad_info.nation){case"日本":e="よろしく，一起去看樱花吗？";break;case"美国":e="Make America Great Again!";break;case"英国":e="想同你一起夜乘伦敦眼！";break;case"俄罗斯":e="干了这瓶伏特加！";break;case"法国":e="C'est La Vie";break;case"德国":e="Die Zeit verging im Fluge.";break;case"澳大利亚":e="一起去大堡礁吧！";break;case"加拿大":e="拾起一片枫叶赠予你";break;case"中国":switch(t=ipLoacation.result.ad_info.province+" "+ipLoacation.result.ad_info.city,ipLoacation.result.ad_info.province){case"北京市":t="北京市",e="北——京——欢迎你~~~";break;case"天津市":t="天津市",e="讲段相声吧。";break;case"重庆市":t="重庆市",e="高德地图:已到达重庆，下面交给百度地图导航。";break;case"河北省":e="山势巍巍成壁垒，天下雄关。铁马金戈由此向，无限江山。";break;case"山西省":e="展开坐具长三尺，已占山河五百余。";break;case"内蒙古自治区":e="天苍苍，野茫茫，风吹草低见牛羊。";break;case"辽宁省":e="我想吃烤鸡架！";break;case"吉林省":e="状元阁就是东北烧烤之王。";break;case"黑龙江省":e="很喜欢哈尔滨大剧院。";break;case"上海市":t="上海市",e="众所周知，中国只有两个城市。";break;case"江苏省":switch(ipLoacation.result.ad_info.city){case"南京市":e="欢迎来自安徽省南京市的小伙伴。";break;case"苏州市":e="上有天堂，下有苏杭。";break;case"泰州市":e="这里也是我的故乡。";break;default:e="散装是必须要散装的。"}break;case"浙江省":e="东风渐绿西湖柳，雁已还人未南归。";break;case"安徽省":e="蚌埠住了，芜湖起飞。";break;case"福建省":e="井邑白云间，岩城远带山。";break;case"江西省":e="落霞与孤鹜齐飞，秋水共长天一色。";break;case"山东省":e="遥望齐州九点烟，一泓海水杯中泻。";break;case"湖北省":e="来碗热干面！";break;case"湖南省":e="74751，长沙斯塔克。";break;case"广东省":e="老板来两斤福建人。";break;case"广西壮族自治区":e="桂林山水甲天下。";break;case"海南省":e="朝观日出逐白浪，夕看云起收霞光。";break;case"四川省":e="康康川妹子。";break;case"贵州省":e="茅台，学生，再塞200。";break;case"云南省":e="玉龙飞舞云缠绕，万仞冰川直耸天。";break;case"西藏自治区":e="躺在茫茫草原上，仰望蓝天。";break;case"陕西省":e="来份臊子面加馍。";break;case"甘肃省":e="羌笛何须怨杨柳，春风不度玉门关。";break;case"青海省":e="牛肉干和老酸奶都好好吃。";break;case"宁夏回族自治区":e="大漠孤烟直，长河落日圆。";break;case"新疆维吾尔自治区":e="驼铃古道丝绸路，胡马犹闻唐汉风。";break;case"台湾省":e="我在这头，大陆在那头。";break;case"香港特别行政区":t="香港特别行政区",e="永定贼有残留地鬼嚎，迎击光非岁玉。";break;case"澳门特别行政区":t="澳门特别行政区",e="性感荷官，在线发牌。";break;default:e="社会主义大法好。"}break;default:e="带我去你的国家逛逛吧。"}let r=new Date;a=r.getHours()>=5&&r.getHours()<11?"<span>上午好</span>，一日之计在于晨，早起的虫儿被鸟吃丫~":r.getHours()>=1&&r.getHours()<13?"<span>中午好</span>，该摸鱼吃午饭了":r.getHours()>=13&&r.getHours()<15?"<span>下午好</span>，懒懒地睡个午觉吧！":r.getHours()>=15&&r.getHours()<16?"<span>三点几啦</span>，饮茶先啦！":r.getHours()>=16&&r.getHours()<19?"<span>夕阳无限好！</span>":r.getHours()>=19&&r.getHours()<24?"<span>晚上好</span>，夜生活嗨起来！":"夜深了，早点休息，少熬夜",document.getElementById("welcome-info").innerHTML=`欢迎来自<span>${t}</span>的小伙伴，${a}<br>你距离半步多约有<span>${s}</span>公里，${e}`}setInterval((()=>{let e=Math.round(new Date("2022/07/12 12:00:00").getTime()/1e3),a=Math.round((new Date).getTime()/1e3)-e,s=new Array(0,0,0,0,0);var t=function(e){return e>9?e:"0"+e};a>=31536e3&&(s[0]=parseInt(a/31536e3),a%=31536e3),a>=86400&&(s[1]=parseInt(a/86400),a%=86400),a>=3600&&(s[2]=t(parseInt(a/3600)),a%=3600),a>=60&&(s[3]=t(parseInt(a/60)),a%=60),a>=0&&(s[4]=t(a));let r="";0!=s[0]&&(r+=s[0]+" 年 "),r+=s[1]+" 天 "+s[2]+" 时: "+s[3]+" 分: "+s[4]+" 秒 ",document.getElementById("runtime").innerHTML=r}),1e3),$.ajax({type:"get",url:"https://apis.map.qq.com/ws/location/v1/ip",data:{key:"5BZBZ-VFQWZ-CCIXF-TNMOI-GPBV3-RFFWJ",output:"jsonp"},dataType:"jsonp",success:function(e){ipLoacation=e,showWelcome()}});
+// runtime设置
+setInterval(() => {
+    // let create_time = Math.round(new Date('2021-10-15 00:00:00').getTime() / 1000); //在此行修改建站时间
+    // 有苹果用户发现safari浏览器不能正常运行，百度了一下发现是格式化的问题，改成下面这种应该就可以了。感谢反馈。
+    let create_time = Math.round(new Date('2022/07/12 12:00:00').getTime() / 1000); //在此行修改建站时间
+    let timestamp = Math.round((new Date().getTime()) / 1000);
+    let second = timestamp - create_time;
+    let time = new Array(0, 0, 0, 0, 0);
+
+    var nol = function(h) {
+        return h > 9 ? h : '0' + h;
+    }
+    if (second >= 365 * 24 * 3600) {
+        time[0] = parseInt(second / (365 * 24 * 3600));
+        second %= 365 * 24 * 3600;
+    }
+    if (second >= 24 * 3600) {
+        time[1] = parseInt(second / (24 * 3600));
+        second %= 24 * 3600;
+    }
+    if (second >= 3600) {
+        time[2] = nol(parseInt(second / 3600));
+        second %= 3600;
+    }
+    if (second >= 60) {
+        time[3] = nol(parseInt(second / 60));
+        second %= 60;
+    }
+    if (second >= 0) {
+        time[4] = nol(second);
+    }
+    let currentTimeHtml = ""
+    if (time[0] != 0) {
+        currentTimeHtml += time[0] + ' 年 '
+    }
+    currentTimeHtml += time[1] + ' 天 ' + time[2] + ' 时: ' + time[3] + ' 分: ' + time[4] + ' 秒 ';
+    document.getElementById("runtime").innerHTML = currentTimeHtml;
+}, 1000);
+
+
+
+
+
+
+
+
+
+
+
+/*公告栏定制 */
+//get请求
+$.ajax({
+    type: 'get',
+    url: 'https://apis.map.qq.com/ws/location/v1/ip',
+    data: {
+        key: '5BZBZ-VFQWZ-CCIXF-TNMOI-GPBV3-RFFWJ',
+        output: 'jsonp',
+    },
+    dataType: 'jsonp',
+    success: function (res) {
+        ipLoacation = res;
+        showWelcome();
+    }
+})
+
+//根据经纬度计算两点距离(点1经度,点1纬度,点2经度,点2纬度)
+function getDistance(e1, n1, e2, n2) {
+    const R = 6371
+    const { sin, cos, asin, PI, hypot } = Math
+
+    let getPoint = (e, n) => {
+        e *= PI / 180
+        n *= PI / 180
+        return { x: cos(n) * cos(e), y: cos(n) * sin(e), z: sin(n) }
+    }
+
+    let a = getPoint(e1, n1)
+    let b = getPoint(e2, n2)
+    let c = hypot(a.x - b.x, a.y - b.y, a.z - b.z)
+    let r = asin(c / 2) * 2 * R
+    return Math.round(r);
+}
+
+
+/*定制 */
+//根据自己的需求定制
+function showWelcome() {
+    if (!document.getElementById("welcome-info")) return
+
+    let dist = getDistance(110.84721, 35.97053, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里记录你自己的经纬度
+
+    let pos = ipLoacation.result.ad_info.nation;
+    let posdesc;
+    //根据国家、省份、城市信息自定义欢迎语
+    //海外地区不支持省份及城市信息
+    switch (ipLoacation.result.ad_info.nation) {
+        case "日本":
+            posdesc = "よろしく，一起去看樱花吗？";
+            break;
+        case "美国":
+            posdesc = "Make America Great Again!";
+            break;
+        case "英国":
+            posdesc = "想同你一起夜乘伦敦眼！";
+            break;
+        case "俄罗斯":
+            posdesc = "干了这瓶伏特加！";
+            break;
+        case "法国":
+            posdesc = "C'est La Vie";
+            break;
+        case "德国":
+            posdesc = "Die Zeit verging im Fluge.";
+            break;
+        case "澳大利亚":
+            posdesc = "一起去大堡礁吧！";
+            break;
+        case "加拿大":
+            posdesc = "拾起一片枫叶赠予你";
+            break;
+        case "中国":
+            pos = ipLoacation.result.ad_info.province + " " + ipLoacation.result.ad_info.city;
+            switch (ipLoacation.result.ad_info.province) {
+                case "北京市":
+                    pos = "北京市";
+                    posdesc = "北——京——欢迎你~~~";
+                    break;
+                case "天津市":
+                    pos = "天津市";
+                    posdesc = "讲段相声吧。";
+                    break;
+                case "重庆市":
+                    pos = "重庆市";
+                    posdesc = "高德地图:已到达重庆，下面交给百度地图导航。"
+                    break;
+                case "河北省":
+                    posdesc = "山势巍巍成壁垒，天下雄关。铁马金戈由此向，无限江山。";
+                    break;
+                case "山西省":
+                    posdesc = "展开坐具长三尺，已占山河五百余。";
+                    break;
+                case "内蒙古自治区":
+                    posdesc = "天苍苍，野茫茫，风吹草低见牛羊。";
+                    break;
+                case "辽宁省":
+                    posdesc = "我想吃烤鸡架！";
+                    break;
+                case "吉林省":
+                    posdesc = "状元阁就是东北烧烤之王。";
+                    break;
+                case "黑龙江省":
+                    posdesc = "很喜欢哈尔滨大剧院。";
+                    break;
+                case "上海市":
+                    pos = "上海市";
+                    posdesc = "众所周知，中国只有两个城市。";
+                    break;
+                case "江苏省":
+                    switch (ipLoacation.result.ad_info.city) {
+                        case "南京市":
+                            posdesc = "欢迎来自安徽省南京市的小伙伴。";
+                            break;
+                        case "苏州市":
+                            posdesc = "上有天堂，下有苏杭。";
+                            break;
+                        case "泰州市":
+                            posdesc = "这里也是我的故乡。";
+                            break;
+                        default:
+                            posdesc = "散装是必须要散装的。";
+                            break;
+                    }
+                    break;
+                case "浙江省":
+                    posdesc = "东风渐绿西湖柳，雁已还人未南归。";
+                    break;
+                case "安徽省":
+                    posdesc = "蚌埠住了，芜湖起飞。";
+                    break;
+                case "福建省":
+                    posdesc = "井邑白云间，岩城远带山。";
+                    break;
+                case "江西省":
+                    posdesc = "落霞与孤鹜齐飞，秋水共长天一色。";
+                    break;
+                case "山东省":
+                    posdesc = "遥望齐州九点烟，一泓海水杯中泻。";
+                    break;
+                case "湖北省":
+                    posdesc = "来碗热干面！";
+                    break;
+                case "湖南省":
+                    posdesc = "74751，长沙斯塔克。";
+                    break;
+                case "广东省":
+                    posdesc = "老板来两斤福建人。";
+                    break;
+                case "广西壮族自治区":
+                    posdesc = "桂林山水甲天下。";
+                    break;
+                case "海南省":
+                    posdesc = "朝观日出逐白浪，夕看云起收霞光。";
+                    break;
+                case "四川省":
+                    posdesc = "康康川妹子。";
+                    break;
+                case "贵州省":
+                    posdesc = "茅台，学生，再塞200。";
+                    break;
+                case "云南省":
+                    posdesc = "玉龙飞舞云缠绕，万仞冰川直耸天。";
+                    break;
+                case "西藏自治区":
+                    posdesc = "躺在茫茫草原上，仰望蓝天。";
+                    break;
+                case "陕西省":
+                    posdesc = "来份臊子面加馍。";
+                    break;
+                case "甘肃省":
+                    posdesc = "羌笛何须怨杨柳，春风不度玉门关。";
+                    break;
+                case "青海省":
+                    posdesc = "牛肉干和老酸奶都好好吃。";
+                    break;
+                case "宁夏回族自治区":
+                    posdesc = "大漠孤烟直，长河落日圆。";
+                    break;
+                case "新疆维吾尔自治区":
+                    posdesc = "驼铃古道丝绸路，胡马犹闻唐汉风。";
+                    break;
+                case "台湾省":
+                    posdesc = "我在这头，大陆在那头。";
+                    break;
+                case "香港特别行政区":
+                    pos = "香港特别行政区";
+                    posdesc = "永定贼有残留地鬼嚎，迎击光非岁玉。";
+                    break;
+                case "澳门特别行政区":
+                    pos = "澳门特别行政区";
+                    posdesc = "性感荷官，在线发牌。";
+                    break;
+                default:
+                    posdesc = "社会主义大法好。";
+                    break;
+            }
+            break;
+        default:
+            posdesc = "带我去你的国家逛逛吧。";
+            break;
+    }
+
+    //判断时间
+    let timeChange;
+    let date = new Date();
+    if (date.getHours()>= 5 && date.getHours() < 11) timeChange = "<span>上午好</span>，一日之计在于晨，早起的虫儿被鸟吃丫~";
+    else if (date.getHours()>= 1 && date.getHours() < 13) timeChange = "<span>中午好</span>，该摸鱼吃午饭了";
+    else if (date.getHours() >= 13 && date.getHours() < 15) timeChange = "<span>下午好</span>，懒懒地睡个午觉吧！";
+    else if (date.getHours() >= 15 && date.getHours() < 16) timeChange = "<span>三点几啦</span>，饮茶先啦！";
+    else if (date.getHours() >= 16 && date.getHours() < 19) timeChange = "<span>夕阳无限好！</span>";
+    else if (date.getHours() >= 19 && date.getHours() < 24) timeChange = "<span>晚上好</span>，夜生活嗨起来！";
+    else timeChange = "夜深了，早点休息，少熬夜";
+
+    //自定义文本需要放的位置
+    document.getElementById("welcome-info").innerHTML = `欢迎来自<span>${pos}</span>的小伙伴，${timeChange}<br>你距离半步多约有<span>${dist}</span>公里，${posdesc}`;
+}
+
+
+
+
+
+
+
+
